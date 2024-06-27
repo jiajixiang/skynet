48d386b02097bdfebd407252b96148176f6acb23
jiajixiang <jia23.jixiang@gmail.com>
2024-06-27 11:40:54 +0800

    feat: loadfile改成load，代码块读取用io文件接口读取

--- src/srv_common/base/import.lua
+++ src/srv_common/base/import.lua
@@ -3,7 +3,7 @@ local table = table
 local pairs = pairs
 _G._ImportModule = _G._ImportModule or {}
 local _ImportModule = _G._ImportModule
-local _PathFile = "../../src/"
+_PathFile = "../../src/"
 local function getClassTbl(Module)
 	local classTbl = {}
 	for k, v in pairs(Module) do
@@ -18,14 +18,16 @@ local function updateImportByContent(pathFile, content)
 	return __updateImportByContent(_ImportModule, pathFile, content)
 end
 
-local function updateImport(PathFile)
-	return __updateImport(_ImportModule, PathFile, loadfile)
-end
-
-local function doImport(PathFile)
+local function doImport(PathFile, PathName)
 	local New = {}
 	setmetatable(New, { __index = _G })
-	local func, err = loadfile(_PathFile .. PathFile, nil, New)
+	local file = io.open(_PathFile .. PathFile, "r")
+	io.input(file)-- 设置默认输入文件
+	local funcString = io.read("*a")
+	local func, err = load(funcString, PathName, nil, New)
+	io.close()
+	
+	-- local func, err = loadfile(_PathFile .. PathFile, nil, New) loadfile存在无法读取最新文件问题
 	if not func then
 		print(string.format("ERROR!!!\n%s\n%s", err, debug.traceback()))
 		return func, err
@@ -34,12 +36,32 @@ local function doImport(PathFile)
 	_ImportModule[PathFile] = New
 	--设置原始环境
 	-- local env = _ENV or _G -- lua5.4,setfenv已废除
-
 	if rawget(New, "__init__") then
 		New:__init__()
 	end
 
 	return New
+end
+
+-- 加载器文件hotloader.lua
+local function hotloader(moduleName, moduleFile)
+    local oldModule = package.loaded[moduleName]
+    local newModule
+ 
+    local f = assert(loadfile(moduleFile))
+    setfenv(f, oldModule and getfenv(oldModule) or _G)
+    newModule = f()
+ 
+    if newModule then
+        for k, v in pairs(newModule) do
+            oldModule[k] = v
+        end
+    end
+end
+
+local function updateImport(PathFile)
+	--doImport(PathFile)
+	return __updateImport(_ImportModule, PathFile, loadfile)
 end
 
 local function SafeImport(PathFile)
@@ -52,7 +74,8 @@ local function SafeImport(PathFile)
 end
 
 function Reimport(PathFile)
-	return doImport(PathFile)
+	local data = doImport(PathFile)
+	return data
 end
 
 function localEnvDoFile(fileName)
